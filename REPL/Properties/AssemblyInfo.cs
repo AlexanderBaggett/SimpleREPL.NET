17f3204c860e27a73bc8ee07b1f665984cd7ce0b
Alexander Baggett <alexander.baggett@gmail.com>
2018-07-18 13:58:31 -0500

    Update AssemblyInfo.cs

--- REPL/Properties/AssemblyInfo.cs
+++ REPL/Properties/AssemblyInfo.cs
@@ -5,19 +5,19 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ColonLang")]
+[assembly: AssemblyTitle("REPL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ColonLang")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("REPL")]
+[assembly: AssemblyCopyright("Copyright ©Alexander Baggett  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e1319e90-0c09-400a-bcca-16f146241b85")]
